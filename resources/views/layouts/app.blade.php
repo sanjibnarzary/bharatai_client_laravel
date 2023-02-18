526928dafe58a2d6e8c050b238e566f8872ac634
Brothereum <o-._.-o@live.com>
2023-02-18 10:16:16 +0530

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Bharat AI') }}</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <script src="{{mix('/js/app.js')}}"></script>
+        <link rel="stylesheet" href="https://getbootstrap.com/docs/5.0/assets/css/docs.css">
+        <!-- Scripts -->
+        <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script src="https://getbootstrap.com/docs/5.0/assets/js/docs.min.js"></script>
+    </body>
+</html>
---
+++ resources/views/translate.blade.php
@@ -0,0 +1,55 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Neural Machine Translation') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+
+                    <div class="row">
+
+                        <div class="col-md-6">
+
+                            <select class="form-control form-control-lg">
+                                <option>Auto Detect</option>
+                                <option>Source Language</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6">
+                            <select class="form-control form-control-lg">
+                                <option>Target Language</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="gap-3"><br></div>
+                    <div class="row">
+                        <div class="col-md-6 ">
+
+                            <textarea class="form-control form-control-lg" rows="5"></textarea>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="bd-clipboard">
+                                <button type="button" class="btn-clipboard" title="" data-bs-original-title="Copy to clipboard">Copy</button>
+                            </div>
+                            <div class="highlight">
+
+                                <div class="chroma">
+                                    <div class="callout-info">
+                                        <code>Destination Language</code>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-6 bg-white border-b border-gray-200 text-center">
+                        <button class="btn btn-lg btn-primary">Translate</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
